51d3704f0eeeaefb28bd9b0e065d17aff70e9146
ZaydenClues <84333982+ZaydenClues@users.noreply.github.com>
2021-11-13 20:45:42 +0530

    updated-player-prefab

---
+++ Assets/Scripts/NavigationController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class NavigationController : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private NavMeshAgent playerNavMeshAgent;
+    [SerializeField] private Transform marker;
+    [SerializeField] private float markerHideDelay = 2f;
+
+    private const float SpeedMultiplier = 0.75f;
+    private Coroutine _displayMarkerCoroutine;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Vector3 position = eventData.pointerPressRaycast.worldPosition;
+        playerNavMeshAgent.SetDestination(position);
+
+        if (_displayMarkerCoroutine != null) StopCoroutine(_displayMarkerCoroutine);
+        _displayMarkerCoroutine = StartCoroutine(DisplayMarker(position, markerHideDelay));
+    }
+
+    private IEnumerator DisplayMarker(Vector3 position, float delay)
+    {
+        marker.position = position;
+        marker.gameObject.SetActive(true);
+        float time = 0;
+        while (time < delay)
+        {
+            marker.position += SpeedMultiplier * Time.deltaTime * Vector3.down;
+            yield return null;
+            time += Time.deltaTime;
+        }
+
+        marker.gameObject.SetActive(false);
+    }
+}
